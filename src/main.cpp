41a2408dbf00341e2c94c701383b86b34a1805e5
mahu <malik.al-hallak@gmx.de>
2015-03-01 18:50:43 +0100

    blabla

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,18 @@
 
 #include "Stereosystem.h"
 
+#include "easylogging++.h"
+
+INITIALIZE_EASYLOGGINGPP
 
 int main(int argc, char* argv[])
 {
 
-	Camera left(NULL);
-	Camera right(NULL);
+	LOG(INFO) << "Application started.";
+	//Camera left(NULL);
+	//Camera right(NULL);
 
-	Stereosystem stereo(left,right);
+	//Stereosystem stereo(left,right);
 
 	std::cout<<"Test"<<std::endl;
 	return 0;
