2602aae95ca14a60ea5af79cc56b8b7f3701821a
mahu <malik.al-hallak@gmx.de>
2015-03-02 12:34:15 +0100

    init cameras and test getImage()

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "Stereosystem.h"
 
@@ -8,12 +9,35 @@ INITIALIZE_EASYLOGGINGPP
 
 int main(int argc, char* argv[])
 {
+	std::string tag = "MAIN\t";
 
-	LOG(INFO) << "Application started.";
-	//Camera left(NULL);
-	//Camera right(NULL);
+	LOG(INFO) << tag << "Application started.";
+	mvIMPACT::acquire::DeviceManager devMgr;    
+		
+	int order = Utility::initCameras(devMgr);
+	std::cout<< order<<std::endl;
+	if(order < 0)
+	{
+		std::cerr <<"Error in camera initialization" << std::endl;
+		LOG(ERROR)<< tag <<"Error in camera initialization" << std::endl;
+	}
+	Camera *left,*right;
 
-	//Stereosystem stereo(left,right);
+	if(order == 0)
+	{
+		left = new Camera(devMgr[0]);
+		right= new Camera(devMgr[1]);	
+	}
+	else if (order == 1)
+	{
+		left = new Camera(devMgr[1]);
+		right = new Camera(devMgr[0]);
+	}
+	
+	Stereosystem stereo(*(left),*(right));
+
+	std::vector<char> test = left->getImage();
+
 
 	std::cout<<"Test"<<std::endl;
 	return 0;
