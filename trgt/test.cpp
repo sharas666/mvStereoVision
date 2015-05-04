1c48136a40385cfcb20bb6f106c58dd3095222ee
hG3n <haghiller@gmail.com>
2015-05-04 17:16:49 +0200

    minor new codesnippets to test stuff

--- trgt/test.cpp
+++ trgt/test.cpp
@@ -16,28 +16,73 @@
 
 INITIALIZE_EASYLOGGINGPP
 
+cv::Mat Q, Q_32F;
+cv::Mat dMapRaw, dMapNorm;
+
+void drawObstacleGrid(cv::Mat &stream, int binning)
+{
+  cv::cvtColor(stream,stream,CV_GRAY2BGR);
+  if(binning == 0) 
+  {
+    //vertical lines
+    cv::line(stream, cv::Point(250,0), cv::Point(250,stream.rows), cv::Scalar(0,0,255), 1);
+    cv::line(stream, cv::Point(502,0), cv::Point(502,stream.rows), cv::Scalar(0,0,255), 1);
+    //horizontal lines
+    cv::line(stream, cv::Point(0,160), cv::Point(stream.cols, 160), cv::Scalar(0,0,255), 1);
+    cv::line(stream, cv::Point(0,320), cv::Point(stream.cols, 320), cv::Scalar(0,0,255), 1);
+  }
+  else
+  {
+    //vertical lines
+    cv::line(stream, cv::Point(250/2,0), cv::Point(250/2,stream.rows), cv::Scalar(0,0,255), 1);
+    cv::line(stream, cv::Point(502/2,0), cv::Point(502/2,stream.rows), cv::Scalar(0,0,255), 1);
+    //horizontal lines
+    cv::line(stream, cv::Point(0,160/2), cv::Point(stream.cols, 160/2), cv::Scalar(0,0,255), 1);
+    cv::line(stream, cv::Point(0,320/2), cv::Point(stream.cols, 320/2), cv::Scalar(0,0,255), 1);
+  }
+}
+
+void mouseClick(int event, int x, int y,int flags, void* userdata)
+{
+  if ( event == CV_EVENT_LBUTTONDOWN )
+  {
+    std::cout << "x: " << x <<"  y: " << y << std::endl;
+  }
+}
+
+void initWindows()
+{
+  cv::namedWindow("dMap" ,1);
+  cv::setMouseCallback("dMap", mouseClick, NULL);
+}
+
 int main(int argc, char const *argv[])
 {
-  
-  cv::Mat dMapRaw, dMapNorm, Q;
-
   cv::FileStorage fs("disparity.yml",cv::FileStorage::READ);
   fs["Q"] >> Q;
   fs["disparity"] >> dMapRaw;
 
+  Q.convertTo(Q_32F,CV_32F);
+
   obstacleDetection obst(dMapRaw, 0);
   obst.buildMeanDistanceMap(Q);
+  std::vector<std::vector<float>> distances = obst.getDistanceMapMean();
 
-  std::vector<Subimage> subvec;
-  subvec = obst.getSubimages();
+  //built the Subimage 'tree'
+  Subimage sub = Subimage(mDispMap, 0);
+  sub.subdivide();
+  std::vector<Subimage> subimage = sub.getSubdividedImages();
+  for (unsigned int i = 0; i < subimage.size(); ++i)
+  {
+    subimage[i].subdivide();
+  }
 
-  cv::Scalar meanCV = subvec[4].getSubdividedImages()[4].calcMeanStdDev().first;
-  float meanSelf = subvec[4].getSubdividedImages()[4].calcMean();
+  subimage[4].getSubdividedImages()[4].getSubMat()
 
-  std::cout << "meanCV:   " << meanCV(0) << std::endl;
-  std::cout << "meanSelf: " << meanSelf << std::endl;
 
   cv::normalize(dMapRaw,dMapNorm,0,255,cv::NORM_MINMAX, CV_8U);
+  initWindows();
+  drawObstacleGrid(dMapNorm,0);
   cv::imshow("dMap", dMapNorm);
   cv::waitKey(0);
 
