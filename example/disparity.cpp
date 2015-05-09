782e72d563a314b9bdeb7d62e474775f7c9be7ce
mahu <malik.al-hallak@gmx.de>
2015-05-09 16:18:29 +0200

    added comments in the examples

--- example/disparity.cpp
+++ example/disparity.cpp
@@ -12,7 +12,7 @@
 INITIALIZE_EASYLOGGINGPP
 
 bool running = true;
-cv::Mat dispMapSGBM, dispMapBM, dispMapNCC;
+cv::Mat dispMapSGBM;
 
 cv::StereoSGBM disparitySGBM;
 
@@ -23,14 +23,6 @@ int exposure = 4800;
 Camera *left;
 Camera *right;
 
-
-double baseline ;
-double fx ;
-double fy ;
-double cx ;
-double cy ;
-double scale = 1;
-
 cv::Mat R, R_32F;
 cv::Mat Q, Q_32F;
 
@@ -39,10 +31,13 @@ std::string qText;
 std::string worldText;
 Stereopair s;
 
+//callback function for opencv slider to change the number of disparity parameter of SGBM
 void changeNumDispSGBM(int, void*)
 {
+  //numDisp has to be dividable by 16, constraint from opencv
   numDispSGBM+=numDispSGBM%16;
 
+  //numDisp is not allowed to be smaller than 16
   if(numDispSGBM < 16)
   {
     numDispSGBM = 16;
@@ -50,32 +45,40 @@ void changeNumDispSGBM(int, void*)
   }
 
   cv::setTrackbarPos("Num Disp", "SGBM", numDispSGBM);
+  //rewrite the disparitySGBM object
   disparitySGBM = cv::StereoSGBM(0,numDispSGBM,windSizeSGBM,8*windSizeSGBM*windSizeSGBM,32*windSizeSGBM*windSizeSGBM);
 }
 
+//callback function for opencv slider to change the window size paramter of SGBM
 void changeWindSizeSGBM(int, void*)
 {
+  //windsize has to be odd...
   if(windSizeSGBM%2 == 0)
     windSizeSGBM+=1;
 
+  //...and bigger than 5
   if(windSizeSGBM < 5)
   {
     windSizeSGBM = 5;
     cv::setTrackbarPos("Wind Size", "SGBM", windSizeSGBM);
   }
   cv::setTrackbarPos("Wind Size", "SGBM", windSizeSGBM);
+  //rewrite the disparitySGBM object
   disparitySGBM = cv::StereoSGBM(0,numDispSGBM,windSizeSGBM,8*windSizeSGBM*windSizeSGBM,32*windSizeSGBM*windSizeSGBM);
 }
 
+//callback function for opencv slider to change the exposure of the cameras
 void setExposure(int, void*)
 {
     left->setExposure(exposure);
     right->setExposure(exposure);
 }
 
+//mouse callback function for opencv
 void mouseClick(int event, int x, int y,int flags, void* userdata)
 {
 
+  //on mouseclick calculate distance of chosen pixel and put it on cout
   if  ( event == CV_EVENT_LBUTTONDOWN )
      {
         cv::Mat_<float>  coordinateQ(1,4);
@@ -84,20 +87,14 @@ void mouseClick(int event, int x, int y,int flags, void* userdata)
             std::cout<<"Coordinate Q: " << coordinateQ <<std::endl;
             std::cout<<"distance Q: " << (coordinateQ(2)/1000.0) <<std::endl;
         }
+        //coordinate is false if the value is negative (e.g.-1) so no information is available at this pint
         else
         {
             std::cout <<"invalid disparity\n";
         }
 
      }
-     // else if  ( event == CV_EVENT_RBUTTONDOWN )
-     // {
-     //      std::cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << std::endl;
-     // }
-     // else if  ( event == CV_EVENT_MBUTTONDOWN )
-     // {
-     //      std::cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << std::endl;
-     // }
+     //on mouseover calculate distance of chosen pixel and put it onto the image
      else if ( event == CV_EVENT_MOUSEMOVE )
      {
         cv::Mat_<float>  coordinateQ(1,4);
@@ -113,25 +110,20 @@ void mouseClick(int event, int x, int y,int flags, void* userdata)
 }
 
 
+//create the windows and slider and register the callback functions
 void initWindows()
 {
     cv::namedWindow("SGBM" ,CV_WINDOW_AUTOSIZE);
     cv::namedWindow("Left" ,CV_WINDOW_AUTOSIZE);
 
-    //cv::namedWindow("BM" ,1);
-
     cv::createTrackbar("Num Disp", "SGBM", &numDispSGBM, 320, changeNumDispSGBM);
     cv::createTrackbar("Wind Size", "SGBM", &windSizeSGBM, 51, changeWindSizeSGBM);
     cv::createTrackbar("Exposure", "SGBM", &exposure, 48000, setExposure);
 
-
-  // cv::createTrackbar("Num Disp", "BM", &numDispBM, 320, changeNumDispBM);
-  // cv::createTrackbar("Wind Size", "BM", &windSizeBM, 51, changeWindSizeBM);
-
-
     cv::setMouseCallback("SGBM", mouseClick, NULL);
     cv::setMouseCallback("Left", mouseClick, NULL);
 }
+
 
 
 int main(int argc, char* argv[])
@@ -141,167 +133,171 @@ int main(int argc, char* argv[])
   LOG(INFO) << tag << "Application started.";
   mvIMPACT::acquire::DeviceManager devMgr;
 
-    initWindows();
+  initWindows();
 
+  //init the cameras
   if(!Utility::initCameras(devMgr,left,right))
   {
     return 0;
   }
 
-  left->setExposure(4800);
-  right->setExposure(4800);
+  //init the stereo system AFTER init the cameras!
+  Stereosystem stereo(left,right);
 
+
+  //collect some configuration parameter written in /configs/default.yml to set "something"
   std::vector<std::string> nodes;
+  //"something" is here the extrinsic and intrinsic parameters...
   nodes.push_back("inputParameter");
+  //... and the location where disparity maps should be stored
   nodes.push_back("capturedDisparities");
 
   std::string config = "./configs/default.yml";
 
   cv::FileStorage fs;
-  left->setExposure(20000);
-  right->setExposure(20000);
 
+  //check if the config has all the collected configuration parameters
   if(!Utility::checkConfig(config,nodes,fs))
   {
   return 0;
   }
 
+  //put the collected paramters to some variable
   std::string inputParameter;
   fs["inputParameter"] >> inputParameter;
 
   std::string outputDirectory;
   fs["capturedDisparities"] >> outputDirectory;
 
-  int imageNumber = 0;
 
+  //load the camera matrices, dist coeffs, R, T, ....
+  if(!stereo.loadIntrinsic(inputParameter+"/intrinsic.yml"))
+    return 0;
+  if(!stereo.loadExtrinisic(inputParameter +"/extrinsic.yml"))
+    return 0;
+
+
+  int imageNumber = 0;
+  //check if the given directory exists
   if(Utility::directoryExist(outputDirectory))
   {
-    std::vector<std::string> tmp1;
-    Utility::getFiles(outputDirectory,tmp1);
+    std::vector<std::string> tmp;
+    //check if the output directory is empty
+    Utility::getFiles(outputDirectory,tmp);
 
-    if(tmp1.size() != 0)
+
+    if(tmp.size() != 0)
     {
+      //if the directory is not empty, clean all files=
       std::cout << "Output directory not empty, clean files? [y/n] " <<std::endl;
       char key = getchar();
       if(key == 'y')
       {
         std::system(std::string("rm " + outputDirectory + "/* -f ").c_str());
       }
+      //start image counter at the last image number in the directory
       else if(key == 'n')
       {
-        imageNumber = tmp1.size();
+        imageNumber = tmp.size();
         LOG(INFO) << tag << "Start with image number " << imageNumber << std::endl;
       }
     }
   }
 
+  //if the output directory doesn't exist->create it
   if(Utility::createDirectory(outputDirectory))
   {
     LOG(INFO) << tag << "Successfully created directory for captured disparity maps." << std::endl;
   }
   else
   {
-    LOG(ERROR) << tag << "Unable to create directoryfor captured disparity maps." <<std::endl;
+    LOG(ERROR) << tag << "Unable to create directory for captured disparity maps." <<std::endl;
     return 0;
   }
 
-  Stereosystem stereo(left,right);
 
-  if(!stereo.loadIntrinsic(inputParameter+"/intrinsic.yml"))
-  return 0;
-  if(!stereo.loadExtrinisic(inputParameter +"/extrinsic.yml"))
-  return 0;
-
+  //the SGBM object
   disparitySGBM = cv::StereoSGBM(0,numDispSGBM,windSizeSGBM,8*windSizeSGBM*windSizeSGBM,32*windSizeSGBM*windSizeSGBM);
-  //disparityBM  = cv::StereoBM(CV_STEREO_BM_BASIC, numDispBM, windSizeBM);
-   stereo.getRectifiedImagepair(s);
 
-    R = stereo.getRotationMatrix();
-    Q = stereo.getQMatrix();
+  stereo.getRectifiedImagepair(s);
 
-    R.convertTo(R_32F,CV_32F);
-    Q.convertTo(Q_32F,CV_32F);
+  R = stereo.getRotationMatrix();
+  Q = stereo.getQMatrix();
 
-    std::cout<<Q_32F<<std::endl;
-    int key = 0;
-    int binning =0;
+  //convert the matrices because otherwise opencv gives you crap
+  R.convertTo(R_32F,CV_32F);
+  Q.convertTo(Q_32F,CV_32F);
 
-    cv::Mat normalizedSGBM;
-    cv::Mat leftColor;
-    while(running)
+  int key = 0;
+  int binning =0;
+
+  cv::Mat normalizedSGBM;
+  cv::Mat leftColor;
+  while(running)
+  {
+    stereo.getRectifiedImagepair(s);
+
+    //convert image to color, just for nice text color on the image
+    cvtColor(s.mLeft, leftColor, CV_GRAY2RGB);
+    //put the string with the distance on the image
+    cv::putText(leftColor,qText.c_str(), cv::Point(10,80),CV_FONT_HERSHEY_PLAIN,2,CV_RGB(255,128,0));
+
+    cv::imshow("Left", leftColor);
+
+    //calculate the disparity
+    Disparity::sgbm(s, dispMapSGBM, disparitySGBM);
+    //normalize it for viewing purposes
+    cv::normalize(dispMapSGBM,normalizedSGBM,0,255,cv::NORM_MINMAX, CV_8U);
+    cv::imshow("SGBM",normalizedSGBM);
+
+    key = cv::waitKey(10);
+    //quit program
+    if(char(key) == 'q')
     {
+      running = false;
+    }
+    //binning mode
+    else if(char(key) == 'b')
+    {
+      if (binning == 0)
+      {
+        binning = 1;
+      }
+      else
+      {
+        binning =0;
+      }
 
-        stereo.getRectifiedImagepair(s);
+      left->setBinning(binning);
+      right->setBinning(binning);
+      stereo.resetRectification();
+    }
+    //capture images
+    else if (char(key) == 'c')
+    {
+      std::string prefix = "";
+      if(imageNumber < 10)
+      {
+        prefix +="000";
+      }
+      else if ((imageNumber >=10) && (imageNumber <100))
+      {
+        prefix += "00";
+      }
+      else if(imageNumber >= 100)
+      {
+        prefix +="0";
+      }
+      cv::normalize(dispMapSGBM,normalizedSGBM,0,255,cv::NORM_MINMAX, CV_8U);
+      cv::imwrite(std::string(outputDirectory+"/sgbm_raw_"+prefix+std::to_string(imageNumber)+".jpg"),dispMapSGBM);
+      cv::imwrite(std::string(outputDirectory+"/sgbm_norm_"+prefix+std::to_string(imageNumber)+".jpg"),normalizedSGBM);
 
-
-        cvtColor(s.mLeft, leftColor, CV_GRAY2RGB);
-        cv::putText(leftColor,qText.c_str(), cv::Point(10,80),CV_FONT_HERSHEY_PLAIN,2,CV_RGB(255,128,0));
-
-        cv::imshow("Left", leftColor);
-//        cv::imshow("Right", s.mRight);
-
-        Disparity::sgbm(s, dispMapSGBM, disparitySGBM);
-        cv::normalize(dispMapSGBM,normalizedSGBM,0,255,cv::NORM_MINMAX, CV_8U);
-        cv::imshow("SGBM",normalizedSGBM);
-
-        key = cv::waitKey(10);
-
-        if(char(key) == 'q')
-        {
-            running = false;
-        }
-        else if(char(key) == 'b')
-        {
-            if (binning == 0)
-            {
-                binning = 1;
-                fx = left->getIntrinsic().at<double>(0,0)/2.0;
-                fy = left->getIntrinsic().at<double>(1,1)/2.0;
-                cx = left->getIntrinsic().at<double>(0,2)/2.0;
-                cy = left->getIntrinsic().at<double>(1,2)/2.0;
-                scale=2;
-            }
-            else
-            {
-                binning =0;
-                fx = left->getIntrinsic().at<double>(0,0);
-                fy = left->getIntrinsic().at<double>(1,1);
-                cx = left->getIntrinsic().at<double>(0,2);
-                cy = left->getIntrinsic().at<double>(1,2);
-                scale=1;
-            }
-
-            left->setBinning(binning);
-            right->setBinning(binning);
-            stereo.resetRectification();
-
-        }
-        else if (char(key) == 'c')
-        {
-            std::string prefix = "";
-            if(imageNumber < 10)
-            {
-                prefix +="000";
-            }
-            else if ((imageNumber >=10) && (imageNumber <100))
-            {
-                prefix += "00";
-            }
-            else if(imageNumber >= 100)
-            {
-                prefix +="0";
-            }
-
-            {
-                cv::normalize(dispMapSGBM,normalizedSGBM,0,255,cv::NORM_MINMAX, CV_8U);
-                cv::imwrite(std::string(outputDirectory+"/sgbm_raw_"+prefix+std::to_string(imageNumber)+".jpg"),dispMapSGBM);
-                cv::imwrite(std::string(outputDirectory+"/sgbm_norm_"+prefix+std::to_string(imageNumber)+".jpg"),normalizedSGBM);
-            }
-            ++imageNumber;
-        }
-        else if(char(key) == 'f')
-            std::cout<<left->getFramerate()<<" "<<right->getFramerate()<<std::endl;
-
+      ++imageNumber;
+    }
+    //get frame rate
+    else if(char(key) == 'f')
+      std::cout<<left->getFramerate()<<" "<<right->getFramerate()<<std::endl;
   }
-  return 0;
+
+return 0;
 }
--- example/images.cpp
+++ example/images.cpp
@@ -9,47 +9,59 @@
 #include <mutex>
 #include <condition_variable>
 
+//init the logger
 INITIALIZE_EASYLOGGINGPP
+
 
 int main(int argc, char* argv[])
 {
+  //tag for LOG-information
   std::string tag = "MAIN\t";
 
   LOG(INFO) << tag << "Application started.";
+
+  //create a device manager for the matrix vision cameras
   mvIMPACT::acquire::DeviceManager devMgr;
 
+  //create cameras
   Camera *left;
   Camera *right;
 
+  //all the driver stuff from matrix vision fo rthe cameras is done here
   if(!Utility::initCameras(devMgr,left,right))
     return 0;
 
 
+  //init the stereo system AFTER init the cameras!
   Stereosystem stereo(left,right);
 
+  //collect some configuration parameter written in /configs/default.yml to set "something"
   std::vector<std::string> nodes;
+  //"something" is here the extrinsic and intrinsic parameters
   nodes.push_back("inputParameter");
+
 
   std::string config = "./configs/default.yml";
 
   cv::FileStorage fs;
 
-
+  //check if the config has all the collected configuration parameters
   if(!Utility::checkConfig(config,nodes,fs))
   {
     return 0;
   }
 
-
+  //put the collected paramters to some variable
   std::string inputParameter;
   fs["inputParameter"] >> inputParameter;
 
-
+  //load the camera matrices, dist coeffs, R, T, ....
   if(!stereo.loadIntrinsic(inputParameter+"/intrinsic.yml"))
     return 0;
   if(!stereo.loadExtrinisic(inputParameter +"/extrinsic.yml"))
     return 0;
 
+  //set the exposure time for left and right camera
   left->setExposure(24000);
   right->setExposure(24000);
 
@@ -58,6 +70,7 @@ int main(int argc, char* argv[])
   bool running = true;
   unsigned int binning = 0;
 
+  //create windows
   cv::namedWindow("Left Distorted" ,1);
   cv::namedWindow("Right Distorted" ,1);
 
@@ -67,19 +80,22 @@ int main(int argc, char* argv[])
   cv::namedWindow("Left Rectified" ,1);
   cv::namedWindow("Right Rectified" ,1);
 
+  //Stereopair is a struct holding a left and right image
   Stereopair distorted;
   Stereopair undistorted;
   Stereopair rectified;
 
 
-
+  //endless loop until 'q' is pressed
   while(running)
   {
 
+    //get the different types of images from the stereo system...
     stereo.getImagepair(distorted);
     stereo.getUndistortedImagepair(undistorted);
     stereo.getRectifiedImagepair(rectified);
 
+    //... and show them all (with the stereopair.mLeft/mRight you have acces to the images and can do whatever you want with them)
     cv::imshow("Left Distorted", distorted.mLeft);
     cv::imshow("Right Distorted", distorted.mRight);
 
@@ -91,26 +107,30 @@ int main(int argc, char* argv[])
 
     key = cv::waitKey(10);
 
+    //quit program
     if(key == 'q')
     {
       running = false;
       break;
     }
+    //switch binning mode
     else if(key == 'b')
     {
     if (binning == 0)
       binning = 1;
     else
       binning =0;
-
-     left->setBinning(binning);
-     right->setBinning(binning);
-     stereo.resetRectification();
+      //after binning the rectificaction has to reseted to have the correct parameters
+      left->setBinning(binning);
+      right->setBinning(binning);
+      stereo.resetRectification();
     }
+    //show FPS of the cameras
     else if(key == 'f')
     {
      std::cout << "FPS: Right "<< left->getFramerate() << " " << "Left " << right->getFramerate() << std::endl;
     }
+    //capture the images anbd write them to disk
     else if (key == 'c')
     {
       cv::imwrite("Left Distorted.jpg",distorted.mLeft);
